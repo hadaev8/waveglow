920a3b10cc3d5dae66b65c0b16bb908c1cb22ff4
Had <had-95@yandex.ru>
2020-05-25 20:45:22 +0300

    fix

--- train.py
+++ train.py
@@ -158,9 +158,11 @@ def train(num_gpus, rank, group_name, output_directory, epochs, learning_rate,
                 loss.backward()
 
             if fp16_run:
-                grad_norm = torch.nn.utils.clip_grad_norm_(amp.master_params(optimizer), 1.0)
+                grad_norm = torch.nn.utils.clip_grad_norm_(
+                    amp.master_params(optimizer), 1.0)
             else:
-                grad_norm = torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+                grad_norm = torch.nn.utils.clip_grad_norm_(
+                    model.parameters(), 1.0)
 
             optimizer.step()
 
@@ -175,8 +177,8 @@ def train(num_gpus, rank, group_name, output_directory, epochs, learning_rate,
                 if rank == 0:
                     checkpoint_path = "{}/waveglow_{}".format(
                         output_directory, iteration)
-                    save_checkpoint(model, optimizer, amp, learning_rate, iteration,
-                                    checkpoint_path)
+                    save_checkpoint(model, optimizer, amp,
+                                    iteration, checkpoint_path)
 
             iteration += 1
 
